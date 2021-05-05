000bed9dc94e329d207d346c611e37b3be3f524c
Raghunandan <raghunandan1541.cse18@chitkara.edu.in>
2021-05-05 11:53:16 +0530

    Codevolution 26 - 28

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import './App.css';
-// import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-// import LifeCycleA from './components/LifeCycleA';
-
-// import './appStyles.css';
-// import styles from './appStyles.module.css';
-// import Form from './component/Form';
-// import MainComponent from './component/MainComponent';
+import RefsDemo from './components/RefsDemo';
 
 function App() {
 	return (
 		<div className="App">
-
-			<Table />
-
-			{/* <FragmentDemo /> */}
-
-			{/* <LifeCycleA /> */}
-			{/* <Form /> */}
-
-			{/* <h1 className='error' >Error</h1>
-			<h1 className={styles.success} >Success</h1> */}
-			
-			{/* <MainComponent /> */}
+			<RefsDemo />
 		</div>
 	);
 }
